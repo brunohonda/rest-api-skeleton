075d59118bc71fead9457922cf712132163ee7a5
Bruno Honda <bruno.honda@4linux.com.br>
2020-06-20 20:43:14 -0300

    Configuração de parser de payload json de requisições

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (request: Request, response: Response) => {
 	return response.json({ message: 'Hello World!!!' });
