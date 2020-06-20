b1bd9a0e6f7a58e292c858bf338301dd888cee5c
Bruno Honda <bruno.honda@4linux.com.br>
2020-06-20 15:18:27 -0300

    Configurado ESLint

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, { Request, Response } from 'express';
 const app = express();
 
 app.get('/', (request: Request, response: Response) => {
-    return response.json({ message: 'Hello World!!!' });
-})
+	return response.json({ message: 'Hello World!!!' });
+});
 
 app.listen(3333);
